1af3a601fc479d1aa3ae141345c9b91764cb0077
Christie Lynam <christielynam@gmail.com>
2018-03-31 13:35:37 -0600

    Remove redux features

--- src/index.js
+++ src/index.js
@@ -2,16 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import rootReducer from './reducers';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
 
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import staffReducer from './staffReducer';
-
-const rootReducer = combineReducers({
-  staff: staffReducer
-})
-
-export default rootReducer;
