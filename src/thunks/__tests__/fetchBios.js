adf342c5331ba478a993716f5c204afb11eb0e1e
Christie Lynam <christielynam@gmail.com>
2019-05-09 15:33:25 -0600

    Add test for fetchBios

--- src/thunks/__tests__/fetchBios.js
+++ src/thunks/__tests__/fetchBios.js
@@ -3,12 +3,27 @@ import { isLoading, hasErrored } from '../../actions'
 
 describe('fetchBios', () => {
   let mockStaffArray
+  let mockStaffMember
   let mockDispatch
 
   beforeEach(() => {
-    mockStaffArray = ['Christie', 'David']
+    mockStaffArray = [{name: 'Christie', info: 'www.linktomoreinfo'}]
+    mockStaffMember = {image: 'some image', bio: 'some bio'}
     mockDispatch = jest.fn()
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      ok: true,
+      json: () => Promise.resolve(mockStaffMember)
+    }))
   })
+
+  it('should call fetch with the correct param', async () => {
+    const thunk = fetchBios(mockStaffArray)
+
+    await thunk(mockDispatch)
+
+    expect(window.fetch).toHaveBeenCalledWith(mockStaffArray[0].info)
+  })
+
 
   it('should dispatch hasErrored with a message if the response is not ok', async () => {
     window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
